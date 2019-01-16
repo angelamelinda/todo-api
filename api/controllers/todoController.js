66ce75264ff360e9ed442bc34f59c452dc204a7b
Angela Melinda <angela.melinda@shopee.com>
2019-01-16 16:31:11 +0700

    Update code

--- api/controllers/todoController.js
+++ api/controllers/todoController.js
@@ -2,62 +2,62 @@ var ObjectID = require('mongodb').ObjectID;
 
 // // get all tasks
 exports.getTasks = (req, res) => {
-    db.collection('todo').find().toArray(function (err, item) {
-      if (err) {
-        res.send({'error':'An error has occurred'});
-      } else {
-        res.send(item);
-      }
-    });
+  db.collection('todo').find().toArray(function (err, item) {
+    if (err) {
+      res.send({ 'error': 'An error has occurred' });
+    } else {
+      res.send(item);
+    }
+  });
 };
 
 exports.getTask = (req, res) => {
-    const id = req.params.id;
-    const details = { '_id': new ObjectID(id) };
-    db.collection('todo').findOne(details, (err, item) => {
-      if (err) {
-        res.send({'error':'An error has occurred'});
-      } else {
-        res.send(item);
-      }
-    });
+  const id = req.params.id;
+  const details = { '_id': new ObjectID(id) };
+  db.collection('todo').findOne(details, (err, item) => {
+    if (err) {
+      res.send({ 'error': 'An error has occurred' });
+    } else {
+      res.send(item);
+    }
+  });
 }
 
 exports.addTask = (req, res) => {
-    const task = {
-      name: req.body.name, status: req.body.status, created_date: req.body.created_date
-    };
-    db.collection('todo').insertOne(task, (err, result) => {
-        if (err) {
-            res.send({ 'error' : 'An error has occured' });
-        } else {
-            res.send(result.ops[0])
-        }
-    })
+  const task = {
+    name: req.body.name, status: req.body.status, created_date: req.body.created_date
+  };
+  db.collection('todo').insertOne(task, (err, result) => {
+    if (err) {
+      res.send({ 'error': 'An error has occured' });
+    } else {
+      res.send(result.ops[0])
+    }
+  })
 }
 
 exports.deleteTask = (req, res) => {
-    const id = req.params.id;
-    const details = { '_id': new ObjectID(id) };
-    db.collection('todo').deleteOne(details, (err, item) => {
-      if (err) {
-        res.send({'error':'An error has occurred'});
-      } else {
-        res.send('Task ' + id + ' deleted!');
-      } 
-    });
+  const id = req.params.id;
+  const details = { '_id': new ObjectID(id) };
+  db.collection('todo').deleteOne(details, (err, item) => {
+    if (err) {
+      res.send({ 'error': 'An error has occurred' });
+    } else {
+      res.send(req.params);
+    }
+  });
 }
 exports.editTask = (req, res) => {
-    const id = req.params.id;
-    const details = { '_id': new ObjectID(id) };
-    const task = {
-        name: req.body.name, status: req.body.status, created_date: req.body.created_date
-    };
-    db.collection('todo').updateOne(details, {$set: task}, (err, result) => {
-      if (err) {
-          res.send({'error':'An error has occurred'});
-      } else {
-          res.send(task);
-      } 
-    });
-  }
+  const id = req.params.id;
+  const details = { '_id': new ObjectID(id) };
+  const task = {
+    name: req.body.name, status: req.body.status, created_date: req.body.created_date
+  };
+  db.collection('todo').updateOne(details, { $set: task }, (err, result) => {
+    if (err) {
+      res.send({ 'error': 'An error has occurred' });
+    } else {
+      res.send(task);
+    }
+  });
+}
